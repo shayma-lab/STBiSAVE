3da3949a1489dc88e43628a9d6fddd7b230996cb
Helmi <helmi@winshot.co-at-992382688482>
2025-05-14 13:43:25 +0100

    fixed transaction category

--- my_first_project/lib/widgets/transaction_tile_wiget.dart
+++ my_first_project/lib/widgets/transaction_tile_wiget.dart
@@ -55,103 +55,104 @@ class _TransactionTileState extends State<TransactionTile> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text("Sélectionner une catégorie"),
-          content: SizedBox(
-            width: double.maxFinite,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  child: categories.isEmpty
-                      ? const Center(child: Text("Aucune catégorie disponible"))
-                      : ListView.builder(
-                          shrinkWrap: true,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          itemCount: categories.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              leading: Icon(
-                                categories[index].icon,
-                                color: categories[index].color,
-                              ),
-                              title: Text(categories[index].title),
-                              trailing: IconButton(
-                                onPressed: () {
-                                  deleteCategory(categories[index].id);
-                                },
-                                icon: Icon(
-                                  Icons.delete,
-                                  color:
-                                      categories[index].userId == widget.user.id
-                                          ? Colors.red
-                                          : Colors.transparent,
+            title: const Text("Sélectionner une catégorie"),
+            content: SizedBox(
+              width: double.maxFinite,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    child: categories.isEmpty
+                        ? const Center(
+                            child: Text("Aucune catégorie disponible"))
+                        : ListView.builder(
+                            shrinkWrap: true,
+                            physics: const AlwaysScrollableScrollPhysics(),
+                            itemCount: categories.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                leading: Icon(
+                                  categories[index].icon,
+                                  color: categories[index].color,
                                 ),
-                              ),
-                              onTap: () async {
-                                try {
-                                  await categoryService
-                                      .updateTransactionCategory(
-                                    widget.transaction.id,
-                                    categories[index].id,
-                                  );
-                                  widget.onCategoryChanged();
-                                  Navigator.of(context).pop();
-                                } catch (e) {
-                                  if(mounted) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text("Erreur: $e"),
-                                        backgroundColor: Colors.red,
-                                      ),
+                                title: Text(categories[index].title),
+                                trailing: IconButton(
+                                  onPressed: () {
+                                    deleteCategory(categories[index].id);
+                                  },
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: categories[index].userId ==
+                                            widget.user.id
+                                        ? Colors.red
+                                        : Colors.transparent,
+                                  ),
+                                ),
+                                onTap: () async {
+                                  try {
+                                    await categoryService
+                                        .updateTransactionCategory(
+                                      widget.transaction.id,
+                                      categories[index].id,
                                     );
+                                    widget.onCategoryChanged();
+                                    Navigator.of(context).pop();
+                                  } catch (e) {
+                                    if (mounted) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          content: Text("Erreur: $e"),
+                                          backgroundColor: Colors.red,
+                                        ),
+                                      );
+                                    }
                                   }
-                                }
-                              },
-                            );
-                          },
-                        ),
-                ),
-                const SizedBox(height: 16),
-                Container(
-                  width: double.infinity,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    gradient: const LinearGradient(
-                      colors: [Color(0xFF0D47A1), Color(0xFF42A5F5)],
-                    ),
+                                },
+                              );
+                            },
+                          ),
                   ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(30),
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) => newCategoryDialog(),
-                        );
-                      },
-                      child: Center(
-                        child: isLoading
-                            ? const CircularProgressIndicator(
-                                color: Colors.white)
-                            : const Text(
-                                "Ajouter une catégorie",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
+            actions: [
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: const LinearGradient(
+                    colors: [Color(0xFF0D47A1), Color(0xFF42A5F5)],
+                  ),
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(30),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => newCategoryDialog(),
+                      );
+                    },
+                    child: Center(
+                      child: isLoading
+                          ? const CircularProgressIndicator(color: Colors.white)
+                          : const Text(
+                              "Ajouter une catégorie",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+            ]);
       },
     );
   }
@@ -248,8 +249,7 @@ class _TransactionTileState extends State<TransactionTile> {
         titleController.text = "";
       });
       Navigator.of(context).pop();
-      Navigator.of(context).pop();
-      _showCategorySelector();
+      fetchCategories();
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
